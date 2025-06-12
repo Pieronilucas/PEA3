890397bd07dcdb1b6724c2ec5fcdea17be1e9971
Lucas Pieroni <lucaspieroni819918@outlook.com>
2025-06-11 22:41:23 -0300

    Adicionar arquivos de projeto.

---
+++ AgendamentoEventos/Controllers/CadastroEventos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendamentoEventos.Controllers
+{
+    public class CadastroEventos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AgendamentoEventos/Controllers/ListagemEventos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendamentoEventos.Controllers
+{
+    public class ListagemEventos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
