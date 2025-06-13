36c6164412872a5d22de0613b394a6bc3c0d01a0
Lucas Pieroni <lucaspieroni819918@outlook.com>
2025-06-13 13:24:43 -0300

    feat: novas features implementando melhorias no visual do cadastro de eventos e possibilitando a entrada de imagens

---
+++ AgendamentoEventos/Controllers/ControleDeEventos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendamentoEventos.Controllers
+{
+    public class ControleDeEventos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ComprarAgora()
+        {
+            return View();
+        }
+
+        public IActionResult VerEvento()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarEventos()
+        {
+            return View();
+        }
+
+    }
+}
